233ab54fa56e8237909924c541bcc21bfa41d941
Harshil-Madaan <59431178+Harshil-Madaan@users.noreply.github.com>
2020-01-03 21:55:22 +0530

    DAY 1_4(PERSONAL DETAILS

---
+++ day 1_4.py
@@ -0,0 +1,14 @@
+'''
+name=input("enter tha name")
+email=input("enter the email")
+phone_number=input("enter the phone_number")
+print("your name is: ",name,"\n your email id is: ",email,"\n your phone number is: " ,phone_number)
+print("the password generated is:happybirthday")
+print(name+email+phone_number)
+'''
+
+
+a=2
+b="code"
+c="I "
+print(c+(a*b))
