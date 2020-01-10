d77f37b7c3dee6267cc644ce47cc49234d10c481
Harshil-Madaan <59431178+Harshil-Madaan@users.noreply.github.com>
2020-01-10 17:09:40 +0530

    list of even and odd numbers

---
+++ day 6_3.py
@@ -0,0 +1,16 @@
+even=[]
+odd=[]
+for i in range(1,11):
+    if(i%2==0):
+        even.append(i)
+    else:
+        odd.append(i)
+        
+print("even list",even)
+print("odd list",odd)
+
+
+
+
+
+
