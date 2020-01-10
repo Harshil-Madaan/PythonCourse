3343f5be20140dcd3fcb78869b8f07ca320c7654
Harshil-Madaan <59431178+Harshil-Madaan@users.noreply.github.com>
2020-01-10 16:20:19 +0530

    STRING METHODS

---
+++ day 4_1.py
@@ -0,0 +1,31 @@
+#a='john'
+#b='Jerry\'s'
+#print ('his name is: %s and he is %s friend.' %(a,b))
+#import string
+str1=("Best in class International Coding institute")
+print(len(str1))
+print(str1.upper())
+print(str1.lower())
+print(str1.find('Coding'))
+#print(str1.replace('in','on'))
+print(str1.replace('on','in'))
+#print(str1.replace('International','national'))
+#print(str1.replace('national','NATIONAL'))
+#print (str1.isalpha())
+a=(str1.split(' '))
+print (a)
+print (a[0])
+print(a[1:3] )
+print (a[0]+a[2])
+print (a[0]+a[3]+a[-1])
+print(' '.join(a))
+print(str1.startswith('Best'))
+print(str1.endswith('coding'))
+
+'''
+
+str1=input("enter the user's name :")
+print(len(str1))
+print(str1.upper())
+print(str1.lower())
+'''
