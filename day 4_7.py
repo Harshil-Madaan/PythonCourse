5341ac29adfd67de153e008323fa793c2c08d07a
Harshil-Madaan <59431178+Harshil-Madaan@users.noreply.github.com>
2020-01-10 16:38:06 +0530

    ASSIGNMENT(STRINGS)

---
+++ day 4_7.py
@@ -0,0 +1,22 @@
+'''a=("Code Techniq Mohali")
+print(a[0])
+print(a[1])
+print(a[5])
+print(a[13])
+print(a[-1])
+print(a[-5])
+print(a[-6])'''
+
+'''a=("Code Techniq Mohali")
+print(a[ :4])
+print(a[4:12])
+print(a[12: ])'''
+
+'''str1=("Don't Give Up")
+print(str1)'''
+
+
+'''str1=("{0} bought some {1} but\n The {1} was {2} so\n betty bought some more \n butter to make the bitter\n butter better" ).format('BETTY','BUTTER','BITTER','BETTER')
+print (str1)'''
+
+
