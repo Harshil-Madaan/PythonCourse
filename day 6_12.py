ac6c913415d50350a0cd177301aae7bf54371bcc
Harshil-Madaan <59431178+Harshil-Madaan@users.noreply.github.com>
2020-01-15 22:16:03 +0530

    FIBONACCI SERIES

---
+++ day 6_12.py
@@ -0,0 +1,11 @@
+#fibonacci series
+a=int(input("Enter the first number of the series"))
+b=int(input("Enter the second number of the series"))
+n=int(input("Enter the number of terms in the series"))
+print(a,b,end=" ")
+while(n-2):
+    c=a+b
+    a=b
+    b=c
+    print(c,end=" ")
+    n=n-1
