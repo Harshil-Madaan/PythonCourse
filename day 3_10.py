62eab7bcb9044cb068bc7ec65f10902fba94690b
Harshil-Madaan <59431178+Harshil-Madaan@users.noreply.github.com>
2020-01-15 23:03:35 +0530

    MOOD (USER INPUT(IF ELIF ELSE))

---
+++ day 3_10.py
@@ -0,0 +1,19 @@
+mood=input("Hello user,How is your mood?")
+if(mood=='happy'):
+    print("wow!enjoy your day.")
+elif(mood=='excited'):
+    print("that's amazing.We hope you have a good day!")
+elif(mood=='sad'):
+    print("it's going to be okay")
+else:
+    print(" okay. have a good day")
+
+
+    
+        
+        
+    
+
+    
+
+    
